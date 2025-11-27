a792201e152b0d664faf15fdf19fc1a1ce2190bf
Yash Mishra <ymishra975@gmail.com>
2025-11-27 12:08:00 +0530

    first commit

---
+++ api/pixel.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+    const YOUR_GOOGLE_SCRIPT_WEBHOOK_URL_HERE = "https://script.google.com/macros/s/AKfycbwwDmBu9FkCBA0Gg3Zis04_K1Qz49QiS5JDKjUR5Bgu6CmrdB_NksVWKziTTmIyEofrPQ/exec"
+    try {
+      const user_id = req.query.id || "unknown";
+      const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+      const ua = req.headers["user-agent"];
+  
+      // --- SEND DATA TO GOOGLE SCRIPT WEBHOOK ---
+      await fetch(YOUR_GOOGLE_SCRIPT_WEBHOOK_URL_HERE, {
+        method: "POST",
+        body: JSON.stringify({
+          user_id,
+          ip,
+          user_agent: ua
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+  
+      // --- RETURN TRACKING PIXEL (1x1 transparent PNG) ---
+      const pixel = Buffer.from(
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8BwQACfsD/Q2w0X8AAAAASUVORK5CYII=",
+        "base64"
+      );
+  
+      res.setHeader("Content-Type", "image/png");
+      res.setHeader("Content-Length", pixel.length);
+      res.status(200).send(pixel);
+  
+    } catch (err) {
+      console.error("Tracker error:", err);
+      res.status(500).send("Error");
+    }
+  }
+  
